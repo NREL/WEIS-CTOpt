de85236466f9cfe4212fc50e342159c6829d8409
yqliaohk <yqliao@umich.edu>
2024-05-08 13:48:54 -0600

    Added static structural analysis

--- weis/owens/openmdao_owens.py
+++ weis/owens/openmdao_owens.py
@@ -9,7 +9,7 @@ import pandas as pd
 # import pickle
 # TODO: remove unnecessary imports when finalizing everything
 # from pathlib import Path
-# from scipy.interpolate                      import PchipInterpolator
+from scipy.interpolate import PchipInterpolator, Akima1DInterpolator
 import openmdao.api as om
 from openmdao.api                           import ExplicitComponent
 # from wisdem.commonse.mpi_tools              import MPI
@@ -48,7 +48,281 @@ from juliacall import Pkg as jlPkg
 #     from mpi4py   import MPI
 
 
-class OWENSSetup(ExplicitComponent):
+class OWENS_WEIS(om.Group):
+
+    def intialize(self):
+        self.options.declare("modeling_options")
+        self.options.declare("analysis_options")
+
+    def setup(self):
+        modeling_opt = self.options["modeling_options"]
+        analysis_opt = self.options["analysis_options"]
+
+        level = modeling_opt['Level']
+
+
+class OWENSStructSetup(ExplicitComponent):
+    def initialize(self):
+        self.options.declare("modeling_options")
+
+    def setup(self):
+        modopt = self.options['modeling_options']
+        OWENS_directory = modopt["OWENS_directory"]
+
+        jlPkg.activate(OWENS_directory)
+        jl.seval("using OWENS")
+        jl.seval("using OWENSAero")
+
+        master_file = modopt["master_input"]
+
+        # Initialize an OWENS model
+        self.model = jl.OWENS.MasterInput(master_file)
+
+        self.analysis_type = modopt["analysis_type"]
+        self.turbine_type = modopt["turbine_type"]
+        self.control_strategy = modopt["control_strategy"]
+        self.aeroModel = modopt["aeroModel"]
+        self.adi_lib = modopt["adi_lib"]
+        self.adi_rootname = modopt["adi_rootname"]
+        self.NumadSpec = modopt["NumadSpec"] # All files go here
+        self.Turbulence = modopt["TurbulenceSpec"] # All files go here
+        self.n_blades = modopt["number_of_blades"]
+        self.structuralModel = modopt["structuralModel"]
+        self.structuralNonlinear = modopt["structuralNonlinear"]
+        self.run_path = modopt["run_path"] # What exactly is this path?
+
+        # Reinitialize the model with the inputs from modeling options
+        self.initialize_model()
+        # Blade inputs, geometry and discretization
+        self.add_input("eta", val=0.5, desc="blade mount point ratio, 0.5 is the blade half chord is perpendicular with the axis of rotation, 0.25 is the quarter chord, etc")
+        self.add_input("Nbld", val=2, desc="number of blades")
+        self.add_input("Blade_Radius", val=17.1)
+        self.add_input("Blade_Height", val=41.9)
+        self.add_input("towerHeight", val=0.5)
+
+
+        # Blade inputs for composites
+        # self.add_input('structuralModel', val='GX', desc="Structural models, GX, TNB, or ROM")
+        # self.add_input('nonlinear', val=False, desc="[]")
+        self.add_input("ntelem", val=10, desc="Tower elements in each")
+        self.add_input("nbelem", val=60, desc="Blade elements in each")
+        self.add_input("ncelem", val=10, desc="Central cable elements in each if turbine type is ARCUS")
+        self.add_input("nselem", val=5, desc="Blade elements in each")
+
+
+        # Solver options (come from modeling options)
+        
+        # Control inputs
+        self.add_input("RPM", val=34.0, desc="RPM")
+        self.add_input("numTS", val=6)
+        self.add_input("delta_t", val=0.05, units="s")
+
+        # Aero parameters
+        self.add_input("NSlices", val=35, desc="number of VAWTAero discritizations #TODO: AD parameters")
+        self.add_input("ntheta", val=30, desc="number of VAWTAero azimuthal discretizations")
+
+        # Environmental conditions
+        # Maybe this fits better into load cases?
+
+        # operation parameters
+        self.add_input("rho", val=0.94, units="kg/m**3", desc="Fluid dendity")
+        self.add_input("Vinf", val=10.1, units="m/s", desc="Inflow velocity")
+
+        # external force
+        self.add_input("Fexternal", val=np.array([1000.0, 0.0, 1000.0]), units="N", desc="External forces")
+
+
+        self.add_output("blade_mass", units="kg", val=0.0, desc="Blade mass breakdown")
+        self.add_output("displacement", units="m", val=0.0)
+
+        # discretization
+        # Maybe these controlpts are grid?
+        # n_control_pts = len()
+        self.add_input("controlpts", val=np.array([3.6479257474344826, 6.226656883619295, 9.082267631309085, 11.449336766507562, 13.310226748873827, 14.781369210504563, 15.8101544043681, 16.566733104331984, 17.011239869982738, 17.167841319391137, 17.04306679619916, 16.631562597633675, 15.923729603782338, 14.932185789551408, 13.62712239754136, 12.075292152969496, 10.252043906945818, 8.124505683235517, 5.678738418596312, 2.8959968657512207]), units=None)
+
+    def initialize_model(self):
+        self.model.analysisType = self.analysis_type
+        self.model.turbineType = self.turbine_type
+        self.model.controlStrategy = self.control_strategy
+        self.model.AModel = self.aeroModel
+        self.model.adi_lib = self.adi_lib
+        self.model.adi_rootname = self.adi_rootname
+        self.model.Nbld = int(self.n_blades)
+
+        # This live in initialize model
+        # TODO: If at any point we want to change the material parameters, we need to take this out to compute
+        self.model.NuMad_geom_xlscsv_file_twr = self.NumadSpec["NuMad_geom_xlscsv_file_twr"]
+        self.model.NuMad_mat_xlscsv_file_twr = self.NumadSpec["NuMad_mat_xlscsv_file_twr"]
+        self.model.NuMad_geom_xlscsv_file_bld = self.NumadSpec["NuMad_geom_xlscsv_file_bld"]
+        self.model.NuMad_mat_xlscsv_file_bld = self.NumadSpec["NuMad_mat_xlscsv_file_bld"]
+        self.model.NuMad_geom_xlscsv_file_strut = self.NumadSpec["NuMad_geom_xlscsv_file_strut"]
+        self.model.NuMad_mat_xlscsv_file_strut = self.NumadSpec["NuMad_mat_xlscsv_file_strut"]
+
+        # Initialize turbulenece
+        self.model.ifw = self.Turbulence["ifw"]
+        self.model.WindType = self.Turbulence["WindType"]
+        self.model.windINPfilename = self.Turbulence["windINPfilename"]
+        self.model.ifw_libfile = self.Turbulence["ifw_libfile"]
+
+        # initialize structural model
+        self.model.structuralModel = self.structuralModel
+        # self.model.nonlinear = self.structuralNonlinear
+
+    def setup_partials(self):
+        self.declare_partials("blade_mass", ["Blade_Radius", "Blade_Height"], method="fd")
+
+    def compute(self, inputs, outputs):
+        path = self.run_path
+        rho = inputs["rho"][0]
+        RPM = inputs["RPM"][0]
+        Vinf = inputs["Vinf"][0]
+        eta = inputs["eta"][0]
+        B = int(inputs["Nbld"][0])
+        H = inputs["Blade_Height"][0]
+        R = inputs["Blade_Radius"][0]
+        Ht = inputs["towerHeight"][0]
+
+        # z_shape1 = collect(LinRange(0,41.9,length(controlpts)+2))
+        # x_shape1 = [0.0;controlpts;0.0]
+        # z_shape = collect(LinRange(0,41.9,60))
+        # x_shape = FLOWMath.akima(z_shape1,x_shape1,z_shape)#[0.0,1.7760245854312287, 5.597183088188207, 8.807794161662574, 11.329376903432605, 13.359580331518579, 14.833606099357858, 15.945156349709, 16.679839160110422, 17.06449826588358, 17.10416552269884, 16.760632435904647, 16.05982913536134, 15.02659565585254, 13.660910465851046, 11.913532434360155, 9.832615229216344, 7.421713825584581, 4.447602800040282, 0.0]
+        # toweroffset = 4.3953443986241725
+        # SNL34_unit_xz = [x_shape;;z_shape]
+        # SNL34x = SNL34_unit_xz[:,1]./maximum(SNL34_unit_xz[:,1])
+        # SNL34z = SNL34_unit_xz[:,2]./maximum(SNL34_unit_xz[:,2])
+        # SNL34Z = SNL34z.*Blade_Height #windio
+        # SNL34X = SNL34x.*Blade_Radius #windio
+        controlpts = inputs["controlpts"]
+        # These are initial grid
+        z_shape1 = np.linspace(0, 41.9, len(controlpts)+2)
+        x_shape1 = np.insert(controlpts,0,0)
+        x_shape1 = np.insert(x_shape1,-1,0)
+        # These are dicretization
+        z_shape = np.linspace(0, 41.9, 60)
+        x_shape = Akima1DInterpolator(z_shape1, x_shape1)(z_shape)
+        SNL34_unit_xz = np.vstack((x_shape, z_shape))
+        SNL34x = SNL34_unit_xz[0,:]/np.max(SNL34_unit_xz[0,:]) # indexing different
+        SNL34z = SNL34_unit_xz[1,:]/np.max(SNL34_unit_xz[1,:])
+        SNL34Z = SNL34z*H #windio
+        SNL34X = SNL34x*R #windio
+
+        shapeY = SNL34Z#collect(LinRange(0,H,Nslices+1))
+        shapeX = SNL34X#R.*(1.0.-4.0.*(shapeY/H.-.5).^2)#shapeX_spline(shapeY)
+
+        ifw = self.Turbulence["ifw"]
+        delta_t = inputs["delta_t"][0]
+        adi_lib = self.adi_lib
+        adi_rootname = self.adi_rootname
+        windINPfilename = self.Turbulence["windINPfilename"]
+        ifw_libfile = self.Turbulence["ifw_libfile"]
+
+        NuMad_geom_xlscsv_file_twr = self.NumadSpec["NuMad_geom_xlscsv_file_twr"]
+        NuMad_mat_xlscsv_file_twr = self.NumadSpec["NuMad_mat_xlscsv_file_twr"]
+        NuMad_geom_xlscsv_file_bld = self.NumadSpec["NuMad_geom_xlscsv_file_bld"]
+        NuMad_mat_xlscsv_file_bld = self.NumadSpec["NuMad_mat_xlscsv_file_bld"]
+        NuMad_geom_xlscsv_file_strut = self.NumadSpec["NuMad_geom_xlscsv_file_strut"]
+        NuMad_mat_xlscsv_file_strut = self.NumadSpec["NuMad_mat_xlscsv_file_strut"]
+
+        numTS = int(inputs["numTS"][0])
+
+        Nslices = int(inputs["NSlices"][0])
+        ntheta = int(inputs["ntheta"][0])
+
+        ntelem = int(inputs["ntelem"][0])
+        nbelem = int(inputs["nbelem"][0])
+        ncelem = int(inputs["ncelem"][0])
+        nselem = int(inputs["nselem"][0])
+
+        AModel = self.aeroModel
+        mesh_type = self.turbine_type
+        setup_outputs = jl.OWENS.setupOWENS(jl.OWENSAero, path, 
+                                            rho=rho,
+                                            Nslices=Nslices,
+                                            ntheta=ntheta,
+                                            RPM=RPM,
+                                            Vinf=Vinf,
+                                            eta=eta,
+                                            B = B,
+                                            H = H,
+                                            R = R,
+                                            shapeY=shapeY,
+                                            shapeX=shapeX,
+                                            ifw=ifw,
+                                            delta_t=delta_t,
+                                            numTS=numTS,
+                                            adi_lib=adi_lib,
+                                            adi_rootname=adi_rootname,
+                                            AD15hubR = 0,
+                                            windINPfilename=windINPfilename,
+                                            ifw_libfile=ifw_libfile,
+                                            NuMad_geom_xlscsv_file_twr=NuMad_geom_xlscsv_file_twr,
+                                            NuMad_mat_xlscsv_file_twr=NuMad_mat_xlscsv_file_twr,
+                                            NuMad_geom_xlscsv_file_bld=NuMad_geom_xlscsv_file_bld,
+                                            NuMad_mat_xlscsv_file_bld=NuMad_mat_xlscsv_file_bld,
+                                            NuMad_geom_xlscsv_file_strut=NuMad_geom_xlscsv_file_strut,
+                                            NuMad_mat_xlscsv_file_strut=NuMad_mat_xlscsv_file_strut,
+                                            Ht=Ht,
+                                            ntelem=ntelem,
+                                            nbelem=nbelem,
+                                            ncelem=ncelem,
+                                            nselem=nselem,
+                                            joint_type = 0,
+                                            strut_mountpointbot = 0.03,
+                                            strut_mountpointtop = 0.03,
+                                            AModel=AModel, #AD, DMS, AC
+                                            DSModel="BV",
+                                            RPI=True,
+                                            cables_connected_to_blade_base = True,
+                                            angularOffset = np.pi/2,
+                                            meshtype = mesh_type
+                                            )
+        
+        top_idx = 23
+        pBC = np.array([[1, 1, 0],
+        [1, 2, 0],
+        [1, 3, 0],
+        [1, 4, 0],
+        [1, 5, 0],
+        [1, 6, 0],
+        [top_idx, 1, 0],
+        [top_idx, 2, 0],
+        [top_idx, 3, 0],
+        [top_idx, 4, 0],
+        [top_idx, 5, 0]])
+
+        mymesh = setup_outputs[0]
+        
+        feamodel = jl.OWENS.FEAModel(analysisType = self.structuralModel,
+                                joint = setup_outputs[3],
+                                platformTurbineConnectionNodeNumber = 1,
+                                pBC=pBC,
+                                nlOn=False,
+                                numNodes = mymesh.numNodes,
+                                numModes = 200,
+                                RayleighAlpha = 0.05,
+                                RayleighBeta = 0.05,
+                                iterationType = "DI")
+        
+        feamodel.analysisType = "S"
+        Fdof = np.array([15,16,17])
+        Fexternal = inputs["Fexternal"]
+
+        Omega = RPM*np.pi/30
+
+        myel = setup_outputs[1]
+
+        displ = np.zeros(mymesh.numNodes*6)
+        elStorage = jl.OWENS.OWENSFEA.initialElementCalculations(feamodel,myel,mymesh)
+        staticAnalysis_outputs = jl.OWENS.OWENSFEA.staticAnalysis(feamodel,mymesh,myel,displ,Omega,0.0,elStorage,reactionNodeNumber=1,Fdof=Fdof,Fexternal=Fexternal)
+        
+        blade_mass = setup_outputs[6]
+        displ = staticAnalysis_outputs[0][-1]
+
+        outputs["blade_mass"] = np.sum(blade_mass)[0,0]
+        outputs["displacement"] = displ
+
+        
+class OWENSUnsteadySetup(ExplicitComponent):
     def initialize(self):
         self.options.declare("modeling_options")
         # self.options.declare("opt_options")
@@ -76,7 +350,7 @@ class OWENSSetup(ExplicitComponent):
         self.analysis_type = modopt["analysis_type"]
         self.turbine_type = modopt["turbine_type"]
         self.control_strategy = modopt["control_strategy"]
-        self.aeroMOodel = modopt["aeroModel"]
+        self.aeroModel = modopt["aeroModel"]
         self.adi_lib = modopt["adi_lib"]
         self.adi_rootname = modopt["adi_rootname"]
         self.NumadSpec = modopt["NumadSpec"] # All files go here
@@ -185,7 +459,7 @@ class OWENSSetup(ExplicitComponent):
         self.model.analysisType = self.analysis_type
         self.model.turbineType = self.turbine_type
         self.model.controlStrategy = self.control_strategy
-        self.model.AModel = self.aeroMOodel
+        self.model.AModel = self.aeroModel
         self.model.adi_lib = self.adi_lib
         self.model.adi_rootname = self.adi_rootname
         self.model.Nbld = int(self.n_blades)
@@ -259,62 +533,6 @@ class OWENSSetup(ExplicitComponent):
         outputs["tower_mass"] = results
         outputs["first_tower_mass"] = results[0]
 
-
-if __name__ == "__main__":
-
-    model = om.Group()
-
-    options = {
-        "OWENS_directory": "/Users/yliao/Library/CloudStorage/OneDrive-NREL/CT-OPT/OWENS",
-        "master_input": "sampleOWENS.yml",
-        "analysis_type": "unsteady",
-        "turbine_type": "Darrieus",
-        "control_strategy": "constantRPM",
-        "aeroModel":"DMS",
-        "adi_lib":"./../../../openfast/build/modules/aerodyn/libaerodyn_inflow_c_binding",
-        "adi_rootname": "./ExampleB",
-        "NumadSpec":{
-            "NuMad_geom_xlscsv_file_twr": "../../../OWENS.jl/docs/src/literate/data/NuMAD_Geom_SNL_5MW_D_TaperedTower.csv",
-            "NuMad_mat_xlscsv_file_twr": "../../../OWENS.jl/docs/src/literate/data/NuMAD_Materials_SNL_5MW.csv",
-            "NuMad_geom_xlscsv_file_bld": "../../../OWENS.jl/docs/src/literate/data/NuMAD_Geom_SNL_5MW_D_Carbon_LCDT_ThickFoils_ThinSkin.csv",
-            "NuMad_mat_xlscsv_file_bld": "../../../OWENS.jl/docs/src/literate/data/NuMAD_Materials_SNL_5MW.csv",
-            "NuMad_geom_xlscsv_file_strut": "../../../OWENS.jl/docs/src/literate/data/NuMAD_Geom_SNL_5MW_Struts.csv",
-            "NuMad_mat_xlscsv_file_strut": "../../../OWENS.jl/docs/src/literate/data/NuMAD_Materials_SNL_5MW.csv",
-
-        },
-        "TurbulenceSpec":{
-            "ifw": False,
-            "WindType": 3,
-            "windINPfilename": "../../../OWENS.jl/docs/src/literate/data/turbsim/115mx115m_30x30_20.0msETM.bts",
-            "ifw_libfile": "./../../../openfast/build/modules/inflowwind/libifw_c_binding"
-        },
-        "number_of_blades": 3,
-        "structuralModel": "GX",
-        "structuralNonlinear": False,
-        "run_path": "../../../OWENS.jl/docs/src/literate/",
-    }
-    model.add_subsystem("crossflow", OWENSSetup(modeling_options=options))
-
-    prob = om.Problem(model)
-    prob.setup()
-    prob.set_val("crossflow.towerHeight", 5.0)
-    prob.run_model()
-    print("Tower height of 5 m: "+str(prob.get_val("crossflow.tower_mass")))
-    prob.set_val("crossflow.towerHeight", 3.0)
-    prob.run_model()
-    print("Tower height of 3 m: "+str(prob.get_val("crossflow.tower_mass")))
-
-    # run opt
-    prob.driver = om.pyOptSparseDriver()
-    prob.driver.options["optimizer"] = "SLSQP"
-
-    prob.model.add_design_var('crossflow.towerHeight', lower=3.0, upper=5.0)
-    prob.model.add_objective('crossflow.first_tower_mass')
-    prob.setup()
-    prob.run_driver()
-
-    print("Optimal height: "+str(prob.get_val("crossflow.towerHeight")))
-    print("This should be 3 m")
 
 
 
