319420c009fdf79aada88aa4774be24b691ceb8b
dzalkind <dzalkind@nrel.gov>
2025-04-21 16:54:00 -0600

    Fix control optimization

--- examples/03_control_opt/control_opt_driver.py
+++ examples/03_control_opt/control_opt_driver.py
@@ -8,9 +8,9 @@ This example uses WEIS to generate the platform model and run OpenFAST simulatio
 
 ## File management
 run_dir                 = os.path.dirname( os.path.realpath(__file__) )
-fname_wt_input          = run_dir + os.sep + 'RM1.yaml'
-fname_modeling_options  = run_dir + os.sep + 'modeling_options_MHK_WISDEM.yaml'
-fname_analysis_options  = run_dir + os.sep + 'analysis_options_control_opt.yaml'
+fname_wt_input          = os.path.join(run_dir,'..','00_setup','RM1.yaml')
+fname_modeling_options  = os.path.join(run_dir,'..','02_platform_opt_openfast','openfast_modeling.yaml')
+fname_analysis_options  = run_dir + os.sep + 'control_opt_analysis.yaml'
 
 
 wt_opt, modeling_options, opt_options = weis_main(fname_wt_input, 
